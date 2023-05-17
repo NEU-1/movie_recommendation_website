0b9af8730b18e042ef1c9d475b59f38e0a14b53c
서지호 <cjsrhd882@naver.com>
2023-05-17 10:54:25 +0900

    기반 코드 오류 수정

--- movie_recommendation/back_server/back_server/settings.py
+++ movie_recommendation/back_server/back_server/settings.py
@@ -32,7 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     # app
-    'coummunity',
+    'accounts',
+    'community',
     # 3rd
     'django_extensions',
     'corsheaders',
@@ -46,8 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'allauth',
     'allauth.account',
-    'allauth.socialaccount',
-    'dj_rest_auth.registration',
+    # 'allauth.socialaccount',
     # 기본
     'django.contrib.admin',
     'django.contrib.auth',
