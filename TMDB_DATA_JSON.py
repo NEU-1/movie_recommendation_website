28eda0a7a671f97ad6479f27b7471c4fef3759d0
서지호 <cjsrhd882@naver.com>
2023-05-18 11:38:05 +0900

    json 데이터 받아오는 코드 수정, actor.json 추가

--- TMDB_DATA_JSON.py
+++ TMDB_DATA_JSON.py
@@ -24,19 +24,14 @@ def fetch_movie_data(url, genres, page, is_active):
     for movie in movies['results']:
         if movie.get('release_date', '') and movie.get('backdrop_path', ''):
             fields = {
-                'adult': movie['adult'],
-                'backdrop_path': movie['backdrop_path'],
-                'original_language': movie['original_language'],
-                'overview': movie['overview'],
-                'popularity': movie['popularity'],
-                'poster_path': movie['poster_path'],
-                'released_date': movie['release_date'],
-                'title': movie['title'],
-                'vote_average': movie['vote_average'],
                 'genres': genre_name(genres, movie['genre_ids']),
-                'is_active': is_active,
-                'director': '',
                 'actors': [],
+                'title': movie['title'],
+                'released_date': movie['release_date'],
+                'popularity': movie['popularity'],
+                'vote_average': movie['vote_average'],
+                'overview': movie['overview'],
+                'poster_path': movie['poster_path'],
             }
             result.append({
                 "pk": movie['id'],
@@ -47,13 +42,20 @@ def fetch_movie_data(url, genres, page, is_active):
 
 
 def fetch_credit_data(movie_data):
+    actor_data = []
     for data in movie_data:
         movie_id = data['pk']
         url = f"https://api.themoviedb.org/3/movie/{movie_id}/credits?api_key={TMDB_API_KEY}"
         credit_info = requests.get(url).json()
-        data['fields']['actors'] = [cast['name'] for cast in credit_info['cast'][:5]]
+        actors = [cast['name'] for cast in credit_info['cast'][:5]]
+        data['fields']['actors'] = actors
         if credit_info['crew']:
             data['fields']['director'] = credit_info['crew'][0]['name']
+        
+        for actor in actors:
+            actor_data.append({"name": actor})
+
+    return actor_data
 
 
 def save_to_file(filename, data):
@@ -63,9 +65,7 @@ def save_to_file(filename, data):
 
 def main():
     genres = fetch_genre_data()
-    genre_data = [{"pk": genre['id'], "model": "movies.genre", "fields": {'name': genre['name']}} for genre in genres]
-    save_to_file("genre", genre_data)
-
+    genre_data = [{"pk": genre['id'], 'name': genre['name']} for genre in genres]
 
     movie_data = []
     for i in range(1, 20):
@@ -75,8 +75,11 @@ def main():
         movie_data.extend(fetch_movie_data(popular_url, genres, i, is_active))
         movie_data.extend(fetch_movie_data(top_rated_url, genres, i, is_active))
 
-    fetch_credit_data(movie_data)
+    actor_data = fetch_credit_data(movie_data)
+    
+    save_to_file("genre", genre_data)
     save_to_file("movies", movie_data)
+    save_to_file("actors", actor_data)
 
 
 if __name__ == "__main__":
