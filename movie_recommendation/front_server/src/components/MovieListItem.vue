e5665e861dce4a6e22f42723a1ec65be920e7c59
서지호 <cjsrhd882@naver.com>
2023-05-18 16:05:06 +0900

    전체 리스트 뷰랑 연결 완료

--- movie_recommendation/front_server/src/components/MovieListItem.vue
+++ movie_recommendation/front_server/src/components/MovieListItem.vue
@@ -1,19 +1,25 @@
 <template>
-  <div class="movie-item">
-    <img :src="movie.poster" :alt="movie.title" />
-    <h3>{{ movie.title }}</h3>
-    <p>{{ movie.description }}</p>
-    <p>아이템입니다</p>
+  <div>
+    <h2>{{ movie.title }}</h2>
+    <img :src="imageUrl" alt="Poster" />
+    <p>{{ movie.overview }}</p>
   </div>
 </template>
 
 <script>
 export default {
-  name: "movieListItem",
+  name: "MovieListItem",
+
   props: {
     movie: {
       type: Object,
       required: true,
+    },
+  },
+  computed: {
+    imageUrl() {
+      const baseUrl = "https://image.tmdb.org/t/p/original";
+      return baseUrl + this.movie.poster_path;
     },
   },
 };
--- movie_recommendation/front_server/src/views/MovieView.vue
+++ movie_recommendation/front_server/src/views/MovieView.vue
@@ -1,19 +1,15 @@
 <template>
   <div>
-    <h2>Movie List</h2>
+    <h2>Movie</h2>
     <div class="movie-list">
-      <p>영화탭입니다</p>
-      <MovieListItem
-        v-for="movie in movies.data"
-        :key="movie.id"
-        :movie="movie"
-      />
+      <MovieList :movies="movies" />
     </div>
   </div>
 </template>
 
 <script>
 import MovieList from "@/components/MovieList.vue";
+import axios from "axios";
 
 export default {
   name: "MovieView",
@@ -27,18 +23,15 @@ export default {
     };
   },
   async created() {
-    const response = await axios.get(
-      "http://127.0.0.1:8000/api/v1/movies/your-url/"
-    );
-    this.movies = response.data;
-  },
-  created() {
-    this.getMovies();
-  },
-  methods: {
-    getMovies() {
-      this.$store.dispatch("getMovies");
-    },
+    try {
+      const response = await axios.get(
+        "http://127.0.0.1:8000/api/v1/movies/your-url/"
+      );
+      this.movies = response.data;
+      console.log(response.data, this.movies);
+    } catch (error) {
+      console.error(error);
+    }
   },
 };
 </script>
