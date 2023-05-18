ad251a5ff78bb9ef3289d3eaf9effd3ec474fd81
김병민 <bejolly@naver.com>
2023-05-18 15:20:37 +0900

    랭킹 기초 작업

---
+++ movie_recommendation/front_server/src/views/MovieRanking.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+      <h1>Movie Ranking</h1>
+      <ul>
+        <li v-for="movie in sortedMovies" :key="movie.pk">
+          <h2>{{ movie.fields.title }}</h2>
+          <p>Rating: {{ movie.fields.vote_average }}</p>
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    data() {
+      return {
+        movies: [],
+      };
+    },
+    mounted() {
+      this.fetchMovies();
+    },
+    computed: {
+      sortedMovies() {
+        return this.movies.sort((a, b) => b.fields.vote_average - a.fields.vote_average);
+      },
+    },
+    methods: {
+      fetchMovies() {
+        axios.get('http://127.0.0.1:8000/api/v1/movies/your-url/')
+          .then(response => {
+            this.movies = response.data;
+          })
+          .catch(error => {
+            console.error(error);
+          });
+      },
+    },
+  };
+  </script>
