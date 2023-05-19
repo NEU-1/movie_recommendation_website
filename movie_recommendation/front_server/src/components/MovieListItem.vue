30f3c409e4b0d83dabcaa3496fd0ae4b8e38423c
서지호 <cjsrhd882@naver.com>
2023-05-19 11:51:10 +0900

    테스트

--- movie_recommendation/front_server/src/components/MovieListItem.vue
+++ movie_recommendation/front_server/src/components/MovieListItem.vue
@@ -1,19 +1,40 @@
 <template>
-  <div>
-    <h2>{{ movie.title }}</h2>
-    <img :src="imageUrl" alt="Poster" />
-    <p>{{ movie.overview }}</p>
+  <div class="movie-item">
+    <div
+      class="poster-container"
+      @mouseover="showDetailsButton = true"
+      @mouseleave="showDetailsButton = false"
+    >
+      <img :src="imageUrl" alt="Poster" class="poster-image" />
+      <router-link
+        v-if="showDetailsButton"
+        class="details-button"
+        :to="`/movie/${movie.id}`"
+      >
+        상세보기 외안됨?
+      </router-link>
+    </div>
+    <h2 class="movie-title">{{ movie.title }}</h2>
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import { mapGetters } from "vuex";
 
 export default {
   data() {
     return {
       movie: {},
+      showDetailsButton: false,
     };
+  },
+  computed: {
+    ...mapGetters(["getMovieById"]),
+    imageUrl() {
+      const baseUrl = "https://image.tmdb.org/t/p/";
+      return baseUrl + this.movie.poster_path;
+    },
   },
   mounted() {
     // 영화 세부 내용을 조회하는 메서드를 호출합니다.
@@ -21,16 +42,17 @@ export default {
   },
   methods: {
     fetchMovieDetails() {
-      const movieId = "영화의 ID를 입력하세요"; // 영화의 ID를 설정하세요
+      const movieId = this.$route.params.movieId;
 
       axios
-        .get(`http://127.0.0.1:8000/api/v1/movies/your-url/${movieId}`)
+        .get(`http://127.0.0.1:8000/api/v1/movies/list/${movieId}`)
         .then((response) => {
-          const movieData = response.data[0]; // 응답이 단일 영화 객체를 포함하는 배열이라고 가정합니다.
+          const movieData = response.data[0];
           this.movie = {
+            id: movieData.id,
             title: movieData.fields.title,
             description: movieData.fields.overview,
-            // 표시할 다른 영화 세부 정보를 추가하세요
+            poster_path: movieData.fields.poster_path, // 포스터 이미지 경로를 가져옵니다.
           };
         })
         .catch((error) => {
@@ -38,21 +60,51 @@ export default {
         });
     },
   },
-  name: "MovieListItem",
-
-  props: {
-    movie: {
-      type: Object,
-      required: true,
-    },
-  },
-  computed: {
-    imageUrl() {
-      const baseUrl = "https://image.tmdb.org/t/p/original";
-      return baseUrl + this.movie.poster_path;
-    },
+  created() {
+    if (!this.getMovieById(this.$route.params.movieId)) {
+      this.fetchMovieDetails();
+    }
   },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.movie-item {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 20px;
+}
+
+.poster-container {
+  position: relative;
+  text-align: center;
+}
+
+.poster-image {
+  width: 360px;
+  height: auto;
+}
+
+.details-button {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  padding: 10px 20px;
+  font-size: 16px;
+  font-weight: bold;
+  color: white;
+  background-color: #000;
+  border: none;
+  border-radius: 4px;
+  opacity: 0.8;
+  cursor: pointer;
+}
+
+.movie-title {
+  font-weight: bold;
+  color: white;
+  margin-top: 10px;
+}
+</style>
