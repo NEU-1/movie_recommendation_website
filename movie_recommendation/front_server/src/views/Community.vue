afbbbd9ee948e6b4fad4b2449beb47dccf2f2410
김상진 <tkdwls0418@naver.com>
2023-05-22 14:45:05 +0900

    커뮤니티 go profile수정

--- movie_recommendation/front_server/src/views/Community.vue
+++ movie_recommendation/front_server/src/views/Community.vue
@@ -2,14 +2,19 @@
   <div>
     <h1>커뮤니티</h1>
     <div class="col-2" @click="goCreate">
-    <button>추가</button></div>
+      <button>추가</button>
+    </div>
     <ul>
       <li v-for="community in communities" :key="community.id">
         <div class="col-3">
-          <p class="m-0 fw-bold fs-5 user" @click="goDetail(community)">{{ community.title }}</p>
+          <p class="m-0 fw-bold fs-5 user" @click="goDetail(community)">
+            {{ community.title }}
+          </p>
         </div>
         <div class="col-2 text-center">
-          <p class="m-0 fs-5 user" @click="goProfile(community)">{{ community.userName }}</p>
+          <p class="m-0 fs-5 user" @click="goProfile(community)">
+            {{ community.userName }}
+          </p>
         </div>
       </li>
     </ul>
@@ -18,39 +23,45 @@
 
 <script>
 export default {
-  name:'CommunityView',
+  name: "CommunityView",
   data() {
     return {
       isLogin: this.$store.getters.isLogin,
-    }
+    };
   },
   computed: {
     communities() {
-      return this.$store.state.communities
-    }
+      return this.$store.state.communities;
+    },
   },
   methods: {
     getCommunityList() {
-      this.$store.dispatch('getCommunityList')
+      this.$store.dispatch("getCommunityList");
     },
     goCreate() {
       if (this.isLogin === false) {
-        alert('게시글을 작성하시려면 로그인을 하세요!')
-        return
+        alert("게시글을 작성하시려면 로그인을 하세요!");
+        return;
       }
-      this.$router.push({ name: 'CommunityCreate' })
+      this.$router.push({ name: "CommunityCreate" });
     },
     goProfile(community) {
-      this.$router.push({ name: 'Profile', params: { user_pk: this.community.user }})
+      this.$router.push({
+        name: "MyProfile",
+        params: { user_pk: community.userId },
+      });
     },
     goDetail(community) {
-  this.$router.push({ name: 'CommunityDetail', params: { community_pk: community.id } })
+      this.$router.push({
+        name: "CommunityDetail",
+        params: { community_pk: community.id },
+      });
     },
   },
   created() {
-    this.getCommunityList()
-  }
-}
+    this.getCommunityList();
+  },
+};
 </script>
 
 <style>
