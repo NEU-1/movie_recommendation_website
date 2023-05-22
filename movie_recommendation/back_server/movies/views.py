cb99103cf231f5246df41020e0c8da669da69764
서지호 <cjsrhd882@naver.com>
2023-05-22 14:39:34 +0900

    디테일 뷰 영상 나오게 수정, JSON 파일 수정, 그에 따른 view들 수정

--- movie_recommendation/back_server/movies/views.py
+++ movie_recommendation/back_server/movies/views.py
@@ -1,45 +1,46 @@
 import json
-from django.shortcuts import render
-from rest_framework.response import Response
-from django.http import HttpResponse, JsonResponse
-from rest_framework import status
-from rest_framework.decorators import api_view, permission_classes
-from django.contrib.auth import get_user_model
-from rest_framework.permissions import IsAuthenticated
-from django.shortcuts import get_object_or_404, get_list_or_404
-from .serializers import MovieSerializer, ReviewSerializer
-# from .serializers import ReviewSerializer, MovieDetailSerializer, GenreSerializer
 from .models import Movie, Genre, Actor, MovieReview
+from django.http import HttpResponse, JsonResponse
+from .serializers import MovieSerializer, ReviewSerializer
+# from .serializers import MovieDetailSerializer, GenreSerializer
+from rest_framework import status
 from django.db.models import Q
+from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.contrib.auth import get_user_model
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 
 @api_view(['GET'])
 def movie_list(request):
-    movies = Movie.objects.all()
-    serializer = MovieSerializer(movies, many=True)
-    return Response(serializer.data)
+    with open('movies/fixtures/movies.json', 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    return JsonResponse(data, safe=False)
 
 @api_view(['GET'])
 def movie_detail(request, movie_id):
-
-    movie = get_object_or_404(Movie.objects.prefetch_related('actors','genres'), movie_id=movie_id)
-
-    serializer = MovieSerializer(movie)
-
+    with open('movies/fixtures/movies.json', 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    movie = None
+    for item in data:
+        if item['pk'] == movie_id:
+            movie = item
+            break
+    if not movie:
+        return Response(status=status.HTTP_404_NOT_FOUND)
     is_liked = False
-
     if request.user:
-        if movie.like_users.filter(pk=request.user.pk).exists():
+        if movie['fields']['actors'] and request.user.pk in movie['fields']['actors']:
             is_liked = True
         else:
             is_liked = False
-
     context = {
-        'data': serializer.data,
+        'data': movie,
         'is_liked': is_liked
     }
-
     return Response(context)
+
 
 @api_view(['GET', 'POST'])
 @permission_classes([IsAuthenticated])
@@ -67,10 +68,6 @@ def movie_likes(request, movie_pk):
     return
 
 
-def your_view_function(request):
-    with open('movies/fixtures/movies.json', 'r', encoding='utf-8') as file:
-        data = json.load(file)
-    return JsonResponse(data, safe=False)
 
 @api_view(['GET'])
 def search_movies(request):
--- movie_recommendation/front_server/src/views/MyProfile.vue
+++ movie_recommendation/front_server/src/views/MyProfile.vue
@@ -5,30 +5,39 @@
         <div class="col-12 d-flex justify-content-around pb-2">
           <h1>{{ user?.username }}</h1>
         </div>
-        <div class="col-sm-12 divider text-center row d-flex justify-content-around" style="margin-bottom:20px">
-            <div class="col-sm-3 col-12 follow-info" @click="openModal(1)">
-              <h2><strong>{{ followersLength }}</strong></h2>                    
-              <p><small>팔로워</small></p>
-            </div>
-            <div class="col-sm-3 col-12 follow-info" @click="openModal(2)">
-              <h2><strong>{{ followingsLength }}</strong></h2>                    
-              <p><small>팔로잉</small></p>
-            </div>
-            <div class="col-sm-3 col-12">
-              <h2><strong>{{ user.like_movies?.length }}</strong></h2>                    
-              <p><small>좋아요한 영화 수</small></p>
-            </div>
-        </div>            
+        <div
+          class="col-sm-12 divider text-center row d-flex justify-content-around"
+          style="margin-bottom: 20px"
+        >
+          <div class="col-sm-3 col-12 follow-info" @click="openModal(1)">
+            <h2>
+              <strong>{{ followersLength }}</strong>
+            </h2>
+            <p><small>팔로워</small></p>
+          </div>
+          <div class="col-sm-3 col-12 follow-info" @click="openModal(2)">
+            <h2>
+              <strong>{{ followingsLength }}</strong>
+            </h2>
+            <p><small>팔로잉</small></p>
+          </div>
+          <div class="col-sm-3 col-12">
+            <h2>
+              <strong>{{ user.like_movies?.length }}</strong>
+            </h2>
+            <p><small>좋아요한 영화 수</small></p>
+          </div>
+        </div>
       </div>
     </div>
-    <br>
+    <br />
   </div>
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 
-const API_URL = 'http://127.0.0.1:8000'
+const API_URL = "http://127.0.0.1:8000";
 
 export default {
   name: "MyProfile",
@@ -38,62 +47,62 @@ export default {
       user: [],
       show1: false,
       show2: false,
-    }
+    };
   },
   created() {
-    this.getMyProfile()
+    this.getMyProfile();
   },
   methods: {
     async getMyProfile() {
       if (this.$store.getters.isLogin === false) {
-        return
+        return;
       }
       try {
-        const me = await this.fetchProfile()
-        const user = await this.fetchName(me.pk)
-        this.me = me
-        this.user = user
+        const me = await this.fetchProfile();
+        const user = await this.fetchName(me.pk);
+        this.me = me;
+        this.user = user;
       } catch (err) {
-        console.error(err)
+        console.error(err);
       }
     },
     fetchProfile() {
       return axios({
-        method: 'get',
+        method: "get",
         url: `${API_URL}/accounts/user/`,
         headers: {
-          Authorization: `Token ${this.$store.state.token}`
+          Authorization: `Token ${this.$store.state.token}`,
         },
-      }).then(res => res.data)
+      }).then((res) => res.data);
     },
     fetchName(my_pk) {
       return axios({
-        method: 'get',
+        method: "get",
         url: `${API_URL}/userinfo/user/${my_pk}/`,
-      }).then(res => res.data)
+      }).then((res) => res.data);
     },
     openModal(modalNumber) {
       if (modalNumber === 1) {
-        this.show1 = true
+        this.show1 = true;
       } else {
-        this.show2 = true
+        this.show2 = true;
       }
     },
     closeModal(modalNumber) {
       if (modalNumber === 1) {
-        this.show1 = false
+        this.show1 = false;
       } else {
-        this.show2 = false
+        this.show2 = false;
       }
     },
   },
   computed: {
     followingsLength() {
-      return this.user.followings?.length || 0
+      return this.user.followings?.length || 0;
     },
     followersLength() {
-      return this.user.followers?.length || 0
+      return this.user.followers?.length || 0;
     },
   },
-}
+};
 </script>
