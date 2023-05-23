1c5cfe85f3908b44feca077755bbef4c9cedc599
NEU-chaldea <cjsrhd882@naver.com>
2023-05-24 01:12:01 +0900

    단또겜 컷

--- movie_recommendation/front_server/src/views/GamePlay.vue
+++ movie_recommendation/front_server/src/views/GamePlay.vue
@@ -1,18 +1,15 @@
 <template>
   <div>
     <SnakeS />
-    <CopyCat />
   </div>
 </template>
 
 <script>
 import SnakeS from "@/components/game/SnakeS.vue";
-import CopyCat from "@/components/game/CopyCat.vue";
 
 export default {
   components: {
     SnakeS,
-    CopyCat,
   }
 }
 </script>
