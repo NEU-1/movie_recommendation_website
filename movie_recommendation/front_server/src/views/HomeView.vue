a7d077c2f45adabfc3485a565905a8da053ec987
서지호 <cjsrhd882@naver.com>
2023-05-23 17:56:51 +0900

    마리오 추가

--- movie_recommendation/front_server/src/views/HomeView.vue
+++ movie_recommendation/front_server/src/views/HomeView.vue
@@ -6,72 +6,74 @@
 
     <SearchBar />
     <Footer />
+    <CodePen />
   </div>
 </template>
 
 <script>
-import SearchBar from "@/components/SearchBar.vue";
+  import SearchBar from "@/components/SearchBar.vue";
+  import CodePen from "@/components/CodePen.vue";
+  import Footer from "@/components/Footer.vue";
+  import img1 from "../assets/존윅.png";
+  import img2 from "../assets/동석.png";
+  import img3 from "../assets/마리오.png";
+  import img4 from "../assets/조커.png";
+  import img5 from "../assets/스즈메.png";
+  import img6 from "../assets/스파이더맨.png";
+  import img7 from "../assets/쥬라기월드.png";
+  import img8 from "../assets/토토로.png";
+  import img9 from "../assets/트랜스포머.png";
+  import img10 from "../assets/해리포터.png";
+  import img11 from "../assets/터미네이터.png";
 
-import Footer from "@/components/Footer.vue";
-import img1 from "../assets/존윅.png";
-import img2 from "../assets/동석.png";
-import img3 from "../assets/마리오.png";
-import img4 from "../assets/조커.png";
-import img5 from "../assets/스즈메.png";
-import img6 from "../assets/스파이더맨.png";
-import img7 from "../assets/쥬라기월드.png";
-import img8 from "../assets/토토로.png";
-import img9 from "../assets/트랜스포머.png";
-import img10 from "../assets/해리포터.png";
-import img11 from "../assets/터미네이터.png";
-
-export default {
-  name: "HomeView",
-  components: {
-    SearchBar,
-    Footer,
-  },
-  data() {
-    return {
-      images: [
-        img1,
-        img2,
-        img3,
-        img4,
-        img5,
-        img6,
-        img7,
-        img8,
-        img9,
-        img10,
-        img11,
-      ],
-    };
-  },
-  computed: {
-    randomImage() {
-      const index = Math.floor(Math.random() * this.images.length);
-      //console.log(this.$store.state.token)
-      return this.images[index];
+  export default {
+    name: "HomeView",
+    components: {
+      SearchBar,
+      Footer,
+      CodePen,
     },
-  },
-};
+    data() {
+      return {
+        images: [
+          img1,
+          img2,
+          img3,
+          img4,
+          img5,
+          img6,
+          img7,
+          img8,
+          img9,
+          img10,
+          img11,
+        ],
+      };
+    },
+    computed: {
+      randomImage() {
+        const index = Math.floor(Math.random() * this.images.length);
+        //console.log(this.$store.state.token)
+        return this.images[index];
+      },
+    },
+  };
 </script>
 
 <style>
-.poster-container {
-  position: relative;
-  display: inline-block;
-  margin-right: 50px;
-}
+  .poster-container {
+    position: relative;
+    display: inline-block;
+    margin-right: 50px;
+  }
 
-.poster-image {
-  width: 70%;
-  height: 50%;
-  transition: transform 0.3s;
-}
+  .poster-image {
+    width: 70%;
+    height: 50%;
+    transition: transform 0.3s;
+  }
 
-.poster-image.hoverable:hover {
-  transform: scale(1.5);
-}
+  .poster-image.hoverable:hover {
+    transform: scale(1.5);
+  }
 </style>
