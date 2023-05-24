98a133c8409d7eacacba5a5d0f21869bc64dfb1f
서지호 <cjsrhd882@naver.com>
2023-05-24 10:46:25 +0900

    좋아요한 영화 사진 출력 추가

--- movie_recommendation/front_server/src/views/ProFile.vue
+++ movie_recommendation/front_server/src/views/ProFile.vue
@@ -24,186 +24,210 @@
       </div>
     </div>
     <button v-if="me && me.id !== user.id" @click="followUser">팔로우</button>
+
+    <!-- 좋아요한 영화 포스트 출력 -->
+    <div v-for="movie in movieList" :key="movie.id">
+      <h3>{{ movie.title }}</h3>
+      <img :src="imageUrl(movie)" alt="Poster">
+    </div>
   </div>
 </template>
 
 <style scoped>
-.proFile {
-  padding: 2rem 0;
-  background-color: rgb(83, 131, 234);
-}
+  .proFile {
+    padding: 2rem 0;
+    background-color: rgb(83, 131, 234);
+  }
 
-.user-heading h1 {
-  font-size: 3rem;
-  color: #343a40;
-}
+  .user-heading h1 {
+    font-size: 3rem;
+    color: #343a40;
+  }
 
-.user-details {
-  margin-top: 3rem;
-}
+  .user-details {
+    margin-top: 3rem;
+  }
 
-.follow-info {
-  background-color: white;
-  padding: 2rem;
-  border-radius: 5px;
-  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
-  flex: 1 0 200px;
-  margin: 1rem;
-  text-align: center;
-}
+  .follow-info {
+    background-color: white;
+    padding: 2rem;
+    border-radius: 5px;
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+    flex: 1 0 200px;
+    margin: 1rem;
+    text-align: center;
+  }
 
-.follow-info h2 {
-  font-size: 2rem;
-  margin-bottom: 1rem;
-}
+  .follow-info h2 {
+    font-size: 2rem;
+    margin-bottom: 1rem;
+  }
 
-.follow-info p {
-  color: #6c757d;
-  font-size: 1.2rem;
-}
+  .follow-info p {
+    color: #6c757d;
+    font-size: 1.2rem;
+  }
 </style>
 
 <script>
-import axios from "axios";
+  import axios from "axios";
 
-const API_URL = "http://127.0.0.1:8000";
+  const API_URL = "http://127.0.0.1:8000";
+  const MOVIE_API_URL = "http://127.0.0.1:8000/api/v1/movies/";
+  const IMAGE_URL = "https://image.tmdb.org/t/p/original";
 
-export default {
-  name: "ProFile",
-  data() {
-    return {
-      me: null,
-      user: null,
-      show1: false,
-      show2: false,
-    };
-  },
-  async created() {
-    await this.getMyProfile();
-    await this.getProfile();
-  },
-  methods: {
-    async getMyProfile() {
-      if (!this.$store.getters.isLogin) {
-        this.me = null;
-        this.user = null;
-        return;
-      }
-
-      try {
-        const me = await this.fetchProfile();
-        if (!me) {
-          console.log('프로필 정보를 받아오지 못했습니다.');
-          return;
-        }
-        const user = await this.fetchName(me.pk);
-        console.log(me)
-        if (!user) {
-          console.log('사용자 정보를 받아오지 못했습니다.');
-          return;
-        }
-        this.me = me;
-        this.user = user;
-      } catch (err) {
-        console.error(err);
-      }
+  export default {
+    name: "ProFile",
+    data() {
+      return {
+        me: null,
+        user: null,
+        show1: false,
+        show2: false,
+        movie: {},
+        movieList: [],
+      };
     },
-    async getProfile() {
-      const userId = this.$route.params.user_pk;
-      console.log(userId, 'getprofile')
-      if (!userId) {
-        console.log('사용자 ID가 없습니다.');
-        this.user = null;
-        return;
-      }
+    async created() {
+      await this.getMyProfile();
+      await this.getProfile();
+      await this.getMovies();
 
-      try {
-        const user = await this.fetchProfile(userId);
-        if (!user) {
-          console.log('사용자 정보를 받아오지 못했습니다.');
+    },
+    methods: {
+      async getMyProfile() {
+        if (!this.$store.getters.isLogin) {
+          this.me = null;
+          this.user = null;
           return;
         }
 
-        this.user = user;
-        console.log(this.user)
-      } catch (err) {
-        console.error(err);
-      }
-    },
-    async fetchProfile(user_pk) {
-      const tokenKey = this.$store.state.token;
-      console.log('Authorization Token Key:', tokenKey);
-      const url = user_pk ? `${API_URL}/user/profile/${user_pk}/` : `${API_URL}/accounts/user/`;
-      console.log(url)
-      try {
-        const res = await axios.get(url, {
-          headers: {
-            Authorization: `Token ${tokenKey}`,
-          },
-        });
-        return res.data;
-      } catch (err) {
-        console.log('프로필 자격 인증 데이터가 없습니다.');
-        return null;
-      }
-    },
-    async fetchName(my_pk) {
-      if (!my_pk) {
-        console.log('사용자 PK가 없습니다.');
-        return null;
-      }
-
-      try {
-        const res = await axios.get(`${API_URL}/user/profile/${my_pk}/`);
-        console.log(my_pk)
-        return res.data;
-      } catch (err) {
-        console.log('네임 자격 인증 데이터가 없습니다.');
-        return null;
-      }
-    },
-    openModal(modalNumber) {
-      if (modalNumber === 1) {
-        this.show1 = true;
-      } else {
-        this.show2 = true;
-      }
-    },
-    closeModal(modalNumber) {
-      if (modalNumber === 1) {
-        this.show1 = false;
-      } else {
-        this.show2 = false;
-      }
-    },
-    followUser() {
-      if (this.me.pk === this.user.id) {
-        alert('자신을 팔로우할 수 없습니다.');
-        return;
-      }
-      axios({
-        url: `${API_URL}/user/follow/${this.me.pk}/${this.user.id}/`,
-        headers: { Authorization: `Token ${this.$store.state.token}` },
-        method: 'POST'
-      }
-      )
-        .then((res) => {
-          if (res.data) {
-            alert('팔로우가 완료되었습니다.');
+        try {
+          const me = await this.fetchProfile();
+          if (!me) {
+            console.log('프로필 정보를 받아오지 못했습니다.');
+            return;
           }
-        })
-        .catch((err) => {
+          const user = await this.fetchName(me.pk);
+          if (!user) {
+            console.log('사용자 정보를 받아오지 못했습니다.');
+            return;
+          }
+          this.me = me;
+          this.user = user;
+        } catch (err) {
           console.error(err);
-        });
+        }
+      },
+      async getProfile() {
+        const userId = this.$route.params.user_pk;
+        if (!userId) {
+          console.log('사용자 ID가 없습니다.');
+          this.user = null;
+          return;
+        }
+
+        try {
+          const user = await this.fetchProfile(userId);
+          if (!user) {
+            console.log('사용자 정보를 받아오지 못했습니다.');
+            return;
+          }
+
+          this.user = user;
+          console.log(this.user)
+        } catch (err) {
+          console.error(err);
+        }
+      },
+      async fetchProfile(user_pk) {
+        const tokenKey = this.$store.state.token;
+        console.log('Authorization Token Key:', tokenKey);
+        const url = user_pk ? `${API_URL}/user/profile/${user_pk}/` : `${API_URL}/accounts/user/`;
+        try {
+          const res = await axios.get(url, {
+            headers: {
+              Authorization: `Token ${tokenKey}`,
+            },
+          });
+          return res.data;
+        } catch (err) {
+          console.log('프로필 자격 인증 데이터가 없습니다.');
+          return null;
+        }
+      },
+      async fetchName(my_pk) {
+        if (!my_pk) {
+          console.log('사용자 PK가 없습니다.');
+          return null;
+        }
+
+        try {
+          const res = await axios.get(`${API_URL}/user/profile/${my_pk}/`);
+          console.log(my_pk)
+          return res.data;
+        } catch (err) {
+          console.log('네임 자격 인증 데이터가 없습니다.');
+          return null;
+        }
+      },
+      openModal(modalNumber) {
+        if (modalNumber === 1) {
+          this.show1 = true;
+        } else {
+          this.show2 = true;
+        }
+      },
+      closeModal(modalNumber) {
+        if (modalNumber === 1) {
+          this.show1 = false;
+        } else {
+          this.show2 = false;
+        }
+      },
+      followUser() {
+        if (this.me.pk === this.user.id) {
+          alert('자신을 팔로우할 수 없습니다.');
+          return;
+        }
+        axios({
+          url: `${API_URL}/user/follow/${this.me.pk}/${this.user.id}/`,
+          headers: { Authorization: `Token ${this.$store.state.token}` },
+          method: 'POST'
+        }
+        )
+          .then((res) => {
+            if (res.data) {
+              alert('팔로우가 완료되었습니다.');
+            }
+          })
+          .catch((err) => {
+            console.error(err);
+          });
+      },
+      async getMovies() {
+        this.movieList = [];
+        for (const movieId of this.user.like_movies) {
+          try {
+            const response = await axios.get(`${MOVIE_API_URL}${movieId}`);
+            this.movieList.push(response.data);
+          } catch (err) {
+            console.error(err);
+          }
+        }
+      },
+      imageUrl(movie) {
+        const baseUrl = "https://image.tmdb.org/t/p/original/";
+        return baseUrl + movie.poster_path;
+      },
     },
-  },
-  computed: {
-    followingsLength() {
-      return this.user ? (this.user.followings?.length || 0) : 0;
+    computed: {
+      followingsLength() {
+        return this.user ? (this.user.followings?.length || 0) : 0;
+      },
+      followersLength() {
+        return this.user ? (this.user.followers?.length || 0) : 0;
+      },
     },
-    followersLength() {
-      return this.user ? (this.user.followers?.length || 0) : 0;
-    },
-  },
-};
+  };
 </script>
