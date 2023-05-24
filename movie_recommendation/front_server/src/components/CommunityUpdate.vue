5735e1125a46890849c342f6d081f7a763a22b61
김상진 <tkdwls0418@naver.com>
2023-05-25 02:20:43 +0900

    커뮤니티 디자인수정및 업데이트폼 디자인 수정

--- movie_recommendation/front_server/src/components/CommunityUpdate.vue
+++ movie_recommendation/front_server/src/components/CommunityUpdate.vue
@@ -1,15 +1,27 @@
 <template>
-    <div>
-      <h1>커뮤니티 글 수정</h1>
-      <form @submit.prevent="updateCommunity">
-        <input v-model.trim="newTitle" type="text" placeholder="제목" />
-        <textarea v-model.trim="newContent" rows="4" cols="50"></textarea>
-        <button type="submit">수정 완료</button>
-      </form>
-      <router-link :to="{ name: 'CommunityDetail', params: { community_pk: communityId }}" class="back-button">뒤로가기</router-link>
+  <div class="container mt-5">
+    <h1 class="text-center mb-4">커뮤니티 글 수정</h1>
+    <div class="row justify-content-center">
+      <div class="col-12 col-md-8">
+        <form @submit.prevent="updateCommunity">
+          <div class="mb-3">
+            <input v-model.trim="newTitle" type="text" class="form-control" placeholder="제목" />
+          </div>
+          <div class="mb-3">
+            <textarea v-model.trim="newContent" rows="20" class="form-control" placeholder="내용을 입력하세요."></textarea>
+          </div>
+          <div class="d-grid gap-2">
+            <button type="submit" class="btn btn-primary">수정 완료</button>
+          </div>
+        </form>
+        <div class="d-grid gap-2 mt-3">
+          <router-link :to="{ name: 'CommunityDetail', params: { community_pk: communityId }}" class="btn btn-secondary">뒤로가기</router-link>
+        </div>
+      </div>
     </div>
-  </template>
-  
+  </div>
+</template>
+
   <script>
   import axios from "axios";
   const API_URL = "http://127.0.0.1:8000";
@@ -49,5 +61,7 @@
   </script>
   
   <style>
-  /* 스타일 설정 */
+  .text-center {
+    color: white;
+  }
   </style>
