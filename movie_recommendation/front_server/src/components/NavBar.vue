fb5c095cff9d8d6a3d8f6b156c16ee84f70573f7
김상진 <tkdwls0418@naver.com>
2023-05-24 09:06:27 +0900

    추천 파트 삭제

--- movie_recommendation/front_server/src/components/NavBar.vue
+++ movie_recommendation/front_server/src/components/NavBar.vue
@@ -1,5 +1,8 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: rgb(83, 131, 234)">
+  <nav
+    class="navbar navbar-expand-lg navbar-dark"
+    style="background-color: rgb(83, 131, 234)"
+  >
     <ul class="navbar-nav">
       <li class="nav-item" :class="{ active: $route.path === '/' }">
         <router-link class="nav-link" to="/">홈</router-link>
@@ -8,58 +11,58 @@
         <router-link class="nav-link" to="/movies">전체영화목록</router-link>
       </li>
       <li class="nav-item" :class="{ active: $route.path === '/ranking' }">
-        <router-link class="nav-link" to="/ranking">랭킹</router-link>
+        <router-link class="nav-link" to="/ranking">추천 랭킹</router-link>
       </li>
       <li class="nav-item" :class="{ active: $route.path === '/community' }">
         <router-link class="nav-link" to="/community">자유게시판</router-link>
       </li>
-      <li class="nav-item" :class="{ active: $route.path === '/recommendation' }">
-        <router-link class="nav-link" to="/recommendation">추천</router-link>
-      </li>
+
       <li class="nav-item" :class="{ active: $route.path === '/profile' }">
-        <router-link class="nav-link" :to="'/profile/' + userId">나의 프로필</router-link>
+        <router-link class="nav-link" :to="'/profile/' + userId"
+          >나의 프로필</router-link
+        >
       </li>
     </ul>
   </nav>
 </template>
 
 <script>
-  export default {
-    name: "NavBar",
-    computed: {
-      userId() {
-        return this.$store.state.userId;
-      }
-    }
-  };
+export default {
+  name: "NavBar",
+  computed: {
+    userId() {
+      return this.$store.state.userId;
+    },
+  },
+};
 </script>
 
 <style scoped>
-  .navbar {
-    padding: 10px;
-  }
+.navbar {
+  padding: 10px;
+}
 
-  .navbar-nav {
-    display: flex;
-    align-items: center;
-  }
+.navbar-nav {
+  display: flex;
+  align-items: center;
+}
 
-  .nav-item {
-    margin-right: 20px;
-  }
+.nav-item {
+  margin-right: 20px;
+}
 
-  .nav-link {
-    color: rgb(80, 76, 76);
-    font-size: 25px;
-    text-decoration: none;
-  }
+.nav-link {
+  color: rgb(80, 76, 76);
+  font-size: 25px;
+  text-decoration: none;
+}
 
-  .nav-link:hover {
-    text-decoration: underline;
-  }
+.nav-link:hover {
+  text-decoration: underline;
+}
 
-  li.active {
-    font-weight: bold;
-    color: #fff;
-  }
+li.active {
+  font-weight: bold;
+  color: #fff;
+}
 </style>
