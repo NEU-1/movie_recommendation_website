2385ee51d7830d8e0d6c001862979d435c8d76be
서지호 <cjsrhd882@naver.com>
2023-05-25 10:00:14 +0900

    프로필 로그인 조건 처리

--- movie_recommendation/front_server/src/components/NavBar.vue
+++ movie_recommendation/front_server/src/components/NavBar.vue
@@ -5,64 +5,61 @@
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav class="navbar-nav">
         <b-nav-item :to="{ path: '/' }" active-class="active">홈</b-nav-item>
-        <b-nav-item :to="{ path: '/movies' }" active-class="active"
-          >전체영화목록</b-nav-item
-        >
-        <b-nav-item :to="{ path: '/ranking' }" active-class="active"
-          >추천 랭킹</b-nav-item
-        >
-        <b-nav-item :to="{ path: '/community' }" active-class="active"
-          >자유게시판</b-nav-item
-        >
-        <b-nav-item
-          :to="{ path: '/profile/' + userId }"
-          active-class="active"
-          v-if="user"
-          >나의 프로필</b-nav-item
-        >
+        <b-nav-item :to="{ path: '/movies' }" active-class="active">전체영화목록</b-nav-item>
+        <b-nav-item :to="{ path: '/ranking' }" active-class="active">추천 랭킹</b-nav-item>
+        <b-nav-item :to="{ path: '/community' }" active-class="active">자유게시판</b-nav-item>
+        <b-nav-item :to="{ path: '/profile/' + username }" active-class="active" v-if="username">나의 프로필</b-nav-item>
       </b-navbar-nav>
     </b-collapse>
   </b-navbar>
 </template>
 
 <script>
-export default {
-  name: "NavBar",
-  computed: {
-    userId() {
-      return this.$store.state.userId;
+  export default {
+    name: "NavBar",
+    data() {
+      return {
+        userData: '',
+      };
     },
-  },
-};
+    computed: {
+      userId() {
+        return this.$store.state.userId;
+      },
+      username() {
+        return this.$store.state.username;
+      },
+    },
+  };
 </script>
 
 <style scoped>
-.custom-navbar {
-  padding: 10px;
-  background-color: rgb(83, 131, 234);
-}
+  .custom-navbar {
+    padding: 10px;
+    background-color: rgb(83, 131, 234);
+  }
 
-.navbar-nav {
-  display: flex;
-  align-items: center;
-}
+  .navbar-nav {
+    display: flex;
+    align-items: center;
+  }
 
-.nav-item {
-  margin-right: 20px;
-}
+  .nav-item {
+    margin-right: 20px;
+  }
 
-.nav-link {
-  color: rgb(80, 76, 76);
-  font-size: 25px;
-  text-decoration: none;
-}
+  .nav-link {
+    color: rgb(80, 76, 76);
+    font-size: 25px;
+    text-decoration: none;
+  }
 
-.nav-link:hover {
-  text-decoration: underline;
-}
+  .nav-link:hover {
+    text-decoration: underline;
+  }
 
-li.active {
-  font-weight: bold;
-  color: #fff;
-}
+  li.active {
+    font-weight: bold;
+    color: #fff;
+  }
 </style>
